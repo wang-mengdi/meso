17fa65747366d529868cb5c69fba5696fc4a0271
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-03-08 16:06:52 -0500

    d cocell can compile

--- src/solver/DifferentialGeometry.h
+++ src/solver/DifferentialGeometry.h
@@ -13,7 +13,7 @@ namespace Meso {
 	// face_y(i,j)=cell(i,j+1)-cell(i,j)
 	// seems that it behaves like cell(i,j)=0 for outside the boundary
 	template<class T>
-	__global__ void D_CoCell_Mapping_Kernel(Grid<2> grid, T* face_x, T* face_y, const T* cell)
+	__global__ void D_CoCell_Mapping_Kernel2(Grid<2> grid, T* face_x, T* face_y, const T* cell)
 	{
 		const int nbx = gridDim.x;
 		const int nby = gridDim.y;
@@ -58,6 +58,66 @@ namespace Meso {
 		if (idy == 7) atomicAdd(face_y + face_ind, -cell_data);
 	}
 
+	// for blockDim = (4, 4, 4)
+	// iterate through cell
+	template<class T>
+	__global__ void D_CoCell_Mapping_Kernel3(Grid<3> grid, T* face_x, T* face_y, T* face_z, const T* cell)
+	{
+		const int nbx = gridDim.x;
+		const int nby = gridDim.y;
+		const int nbz = gridDim.z;
+
+		const int bx = blockIdx.x;
+		const int by = blockIdx.y;
+		const int bz = blockIdx.z;
+
+		const int idx = threadIdx.x;
+		const int idy = threadIdx.y;
+		const int idz = threadIdx.z;
+
+		__shared__ T shared_cell_data[64];
+
+		// 8x8 cell data load
+		shared_cell_data[(idz * 4 + idy) * 4 + idx] = cell[grid.Index(Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + idz))];
+		__syncthreads();
+
+		const T cell_data = shared_cell_data[(idz * 4 + idy) * 4 + idx];
+		int face_ind;
+
+		// x-axis faces
+		face_ind = grid.Face_Index(0, Vector3i(bx * 4 + (idx + 1), by * 4 + idy, bz * 4 + idz));
+		if (idx != 3) face_x[face_ind] = shared_cell_data[(idz * 4 + idy) * 4 + (idx + 1)] - cell_data;
+
+		// y-axis faces
+		face_ind = grid.Face_Index(1, Vector3i(bx * 4 + idx, by * 4 + (idy + 1), bz * 4 + idz));
+		if (idy != 3) face_y[face_ind] = shared_cell_data[(idz * 4 + (idy + 1)) * 4 + idx] - cell_data;
+
+		// z-axis faces
+		face_ind = grid.Face_Index(2, Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + (idz + 1)));
+		if (idz != 3) face_z[face_ind] = shared_cell_data[((idz + 1) * 4 + idy) * 4 + idx] - cell_data;
+
+		// x-axis shared faces
+		face_ind = grid.Face_Index(0, Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + idz));
+		if (idx == 0) atomicAdd(face_x + face_ind, cell_data);
+
+		face_ind = grid.Face_Index(0, Vector3i(bx * 4 + (idx + 1), by * 4 + idy, bz * 4 + idz));
+		if (idx == 3) atomicAdd(face_x + face_ind, -cell_data);
+
+		// y-axis shared faces
+		face_ind = grid.Face_Index(1, Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + idz));
+		if (idy == 0) atomicAdd(face_y + face_ind, cell_data);
+
+		face_ind = grid.Face_Index(1, Vector3i(bx * 4 + idx, by * 4 + (idy + 1), bz * 4 + idz));
+		if (idy == 3) atomicAdd(face_y + face_ind, -cell_data);
+
+		// z-axis shared faces
+		face_ind = grid.Face_Index(2, Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + idz));
+		if (idz == 0) atomicAdd(face_z + face_ind, cell_data);
+
+		face_ind = grid.Face_Index(2, Vector3i(bx * 4 + idx, by * 4 + idy, bz * 4 + (idz + 1)));
+		if (idz == 3) atomicAdd(face_z + face_ind, -cell_data);
+	}
+
 	//diff of C, store on F
 	template<class T, int d>
 	void D_CoCell_Mapping(const Field<T,d,DataHolder::DEVICE> &C, FaceField<T,d,DataHolder::DEVICE> &F)
@@ -69,7 +129,16 @@ namespace Meso {
 			T* face_y = thrust::raw_pointer_cast(F.face_data[1].data());
 			const T* cell = thrust::raw_pointer_cast(C.data.data());
 
-			D_CoCell_Mapping_Kernel << <dim3((Nx >> 3), (Ny >> 3)), dim3(8, 8) >> > (C.grid, face_x, face_y, cell);
+			D_CoCell_Mapping_Kernel2 << <dim3((Nx >> 3), (Ny >> 3)), dim3(8, 8) >> > (C.grid, face_x, face_y, cell);
+		}
+		else if constexpr (d == 3) {
+			int Nx = C.grid.counts[0], Ny = C.grid.counts[1], Nz = C.grid.counts[2];
+			T* face_x = thrust::raw_pointer_cast(F.face_data[0].data());
+			T* face_y = thrust::raw_pointer_cast(F.face_data[1].data());
+			T* face_z = thrust::raw_pointer_cast(F.face_data[2].data());
+			const T* cell = thrust::raw_pointer_cast(C.data.data());
+
+			D_CoCell_Mapping_Kernel3 << <dim3((Nx >> 2), (Ny >> 2), (Nz >> 2)), dim3(4, 4, 4) >> > (C.grid, face_x, face_y, face_z, cell);
 		}
 	}
 
