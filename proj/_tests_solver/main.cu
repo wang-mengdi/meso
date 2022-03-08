669d70910269be57a759f1a8f5a192d437b45ab7
wmdcstdio <mengdi.wang.gr@dartmouth.edu>
2022-03-08 02:42:53 -0500

    poisson diagonal failed

--- proj/_tests_solver/main.cu
+++ proj/_tests_solver/main.cu
@@ -20,7 +20,8 @@ int main(){
 
     //Test_Sparse_Matrix();
 
-    Test_Poisson_Diagonal<real>(Vector2i(14, 53));
+    //Test_Poisson_Diagonal<real>(Vector2i(14, 53));
+    Test_Poisson_Diagonal<real>(Vector2i(8, 8));
     
 
     return 0;
