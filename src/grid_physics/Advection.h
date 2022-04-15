df80649781619559a704794a64a1baf316da7ff8
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-04-15 15:25:13 -0400

    try to add physics

---
+++ src/grid_physics/Advection.h
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////
+// Advection algorithms method
+// Copyright (c) (2022-), Mengdi Wang
+// This file is part of MESO, whose distribution is governed by the LICENSE file.
+//////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Field.h"
+#include "FaceField.h"
+
+namespace Meso {
+	template<int d>
+	class Advection {
+		static Advect() {
+
+		}
+	};
+}
