b9c07d1928cd2c265c2669d23e63206d6e10d82c
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-04-19 17:11:08 -0400

    can compile without c++20

--- src/common/Json.h
+++ src/common/Json.h
@@ -57,22 +57,22 @@ namespace Meso {
 	namespace Json {
 		template<class T>
 		T Value(json& j, const std::string key, const T default_value) {
-			//if (j.contains(key)) {
-			//	T value = j.at(key);
-			//	fmt::print(fg(fmt::color::green), "#     [=] Parse key ");
-			//	fmt::print("{}", key);
-			//	fmt::print(fg(fmt::color::green), " from json: ");
-			//	fmt::print("{}\n", value);
-			//	return value;
-			//}
-			//else {
-			//	j[key] = default_value;
-			//	fmt::print(fg(fmt::color::yellow), "#     [+] Can't parse key ");
-			//	fmt::print("{}", key);
-			//	fmt::print(fg(fmt::color::yellow), " in json, set to default value: ");
-			//	fmt::print("{}\n", default_value);
-			//	return default_value;
-			//}
+			if (j.contains(key)) {
+				T value = j.at(key);
+				fmt::print(fg(fmt::color::green), "#     [=] Parse key ");
+				fmt::print("{}", key);
+				fmt::print(fg(fmt::color::green), " from json: ");
+				fmt::print("{}\n", value);
+				return value;
+			}
+			else {
+				j[key] = default_value;
+				fmt::print(fg(fmt::color::yellow), "#     [+] Can't parse key ");
+				fmt::print("{}", key);
+				fmt::print(fg(fmt::color::yellow), " in json, set to default value: ");
+				fmt::print("{}\n", default_value);
+				return default_value;
+			}
 		}
 
 		template<class T>
