43d04e770dee65e09e006b0b5a12e4ab5564ce95
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-04-20 15:57:43 -0400

    boundary condition for field

--- src/reservoir/BoundaryCondition.h
+++ src/reservoir/BoundaryCondition.h
@@ -16,19 +16,31 @@ namespace Meso {
 		virtual void Copy_Masked(DataStructure& dest, const DataStructure& src) = 0;
 	};
 
+	template<class DataStructure>
+	class BoundaryConditionDirect : BoundaryConditionBase<DataStructure> {
+	public:
+	};
+
 	template<class T, int d, DataHolder side>
-	class BoundaryConditionDirect : BoundaryConditionBase<Field<T, d, side>> {
+	class BoundaryConditionDirect<Field<T, d, side>> : BoundaryConditionBase<Field<T, d, side>> {
 	public:
 		Field<bool, d, side> fixed;
 		template<DataHolder side1>
 		void Init(const Field<bool, d, side1>& _fixed) {
 			fixed = _fixed;
 		}
-		void Copy_Masked(Field<T, d, side>& dest, const Field<T, d, side>& src) {
+		virtual void Copy_Masked(Field<T, d, side>& dest, const Field<T, d, side>& src) {
 			ArrayFunc::Copy_Masked(dest.Data(), src.Data(), fixed.Data());
 		}
-		void Set_Masked(Field<T, d, side>& dest, const T val) {
-			ArrayFunc::Copy_Masked(dest.Data(), thrust::make_constant_iterator(val), fixed.Data());
+		virtual void Set_Masked(Field<T, d, side>& dest, const T val) {
+			thrust::transform_if(
+				dest.Data().begin(),//first
+				dest.Data().end(),//last
+				fixed.Data().begin(),//stencil
+				dest.Data().begin(),//result
+				[=]__device__(const T dest_val) { return val; },//op
+				thrust::identity<bool>()//pred
+			);
 		}
 	};
 }
