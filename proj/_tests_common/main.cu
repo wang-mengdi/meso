b3ce97b34a1b9c4f8df4523c1d3740d0a25b5a46
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-04-29 18:06:28 -0400

    try to config run_project

--- proj/_tests_common/main.cu
+++ proj/_tests_common/main.cu
@@ -3,13 +3,14 @@ using namespace Meso;
 
 
 int main() {
-    int n = 10;
-    SparseMatrix<double> A = MatrixXd::Random(n, n).sparseView(0.5, 1);
-    VectorXd b(n), x(n);
-    //Eigen::ConjugateGradient<SparseMatrix<double>, Eigen::Lower | Eigen::Upper, Eigen::IncompleteCholesky<double, Eigen::Lower | Eigen::Upper>> cg;
-    Eigen::ConjugateGradient<SparseMatrix<double>, Eigen::Lower, Eigen::DiagonalPreconditioner<double>> cg;
-    cg.compute(A);
-    x = cg.solve(b);
-    x = cg.solve(b);
+    Info("Hello world!");
+    //int n = 10;
+    //SparseMatrix<double> A = MatrixXd::Random(n, n).sparseView(0.5, 1);
+    //VectorXd b(n), x(n);
+    ////Eigen::ConjugateGradient<SparseMatrix<double>, Eigen::Lower | Eigen::Upper, Eigen::IncompleteCholesky<double, Eigen::Lower | Eigen::Upper>> cg;
+    //Eigen::ConjugateGradient<SparseMatrix<double>, Eigen::Lower, Eigen::DiagonalPreconditioner<double>> cg;
+    //cg.compute(A);
+    //x = cg.solve(b);
+    //x = cg.solve(b);
     return 0;
 }
---
+++ run_project.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import platform
+
+if __name__=='__main__':
+    assert (len(sys.argv)==2), 'Error: must pass 1 parameter(s). Example: python make_project.py _tests_reservoir'
+    proj_name=sys.argv[1]
+    bin_dir=os.path.join('..','..','bin',proj_name)
+    build_dir=os.path.join('..','..','build',proj_name)
+    lua_file=os.path.join('proj',proj_name,'xmake.lua')
+    proj_dir=os.path.join('proj',proj_name)
+    clean_cmd="xmake c -P {} -a".format(proj_dir)
+    config_cmd="xmake f -o {} -P {} -y".format(bin_dir, proj_dir)
+    project_cmd="xmake project -k vsxmake -v -a \"x64\" -P {} {}".format(proj_dir, build_dir)
+    print(clean_cmd)
+    os.system(clean_cmd)
+    print(config_cmd)
+    os.system(config_cmd)
+    print(project_cmd)
+    os.system(project_cmd)
