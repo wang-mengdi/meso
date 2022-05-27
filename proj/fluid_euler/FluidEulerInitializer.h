945345ee060af9243d06cdbde9ad3e9ec92e8648
Wanxin-Hu <wanxin.hu.25@dartmouth.edu>
2022-05-27 17:02:07 -0400

    add taylor vortex, need to work on poisson and velocity setting

--- proj/fluid_euler/FluidEulerInitializer.h
+++ proj/fluid_euler/FluidEulerInitializer.h
@@ -28,6 +28,7 @@ namespace Meso {
 			case 0:Case_0(j, fluid); break;
 			case 1:Case_1(j, fluid); break;
 			case 2:Case_2(j, fluid); break;
+			case 3:Case_3(j, fluid); break;
 			default:Assert(false, "test {} not exist", test); break;
 			}
 		}
@@ -113,23 +114,77 @@ namespace Meso {
 						initial_vel(axis, face) = 0.0;
 						vol(axis, face) = 0;
 					}
-					/*else {
-						initial_vel(axis, face) = 1.0;
-					}*/
+				}
+			);
+			fluid.Init(fixed, vol, face_fixed, initial_vel);
+		}
+
+
+		// taylor vortex
+		void Case_3(json& j, FluidEuler<d>& fluid) {
+			int scale = Json::Value(j, "scale", 32);
+			real dx = 1.0 / scale;
+			VectorDi grid_size = scale * MathFunc::Vi<d>(1, 1, 1);
+			Grid<d> grid(grid_size, dx, VectorD::Zero(), MAC);
+
+			Eigen::Matrix<int, 3, 2> bc_width;
+			Eigen::Matrix<real, 3, 2> bc_val;
+			bc_width << 0, 0, 0, 0, 0, 0;
+			bc_val << 0, 0, 0, 0, 0, 0;
+
+			GridEulerFunc::Set_Boundary(grid, bc_width, bc_val, fixed, vol, face_fixed, initial_vel);
+
+			VectorD vortex_p1, vortex_p2;
+
+			// two vortices are 0.81 apart
+			vortex_p1[0] = grid.Center()[0] + (real)0.405;
+			vortex_p2[0] = grid.Center()[0] - (real)0.405;
+			vortex_p1[1] = grid.Center()[1]; 
+			vortex_p2[1] = vortex_p1[1];
+
+			//Set_Velocity_Taylor_Vortex(vortex_p1, vortex_p2);
+			Field<real, d> wz;
+			//wz.Resize(grid.cell_counts);
+
+			grid.Exec_Nodes(
+				[&](const VectorDi cell) {
+					const VectorD pos = grid.Position(cell);
+					real pr1 = pow((pos[0] - vortex_p1[0]), 2) + pow((pos[1] - vortex_p1[1]), 2);
+					real pr2 = pow((pos[0] - vortex_p2[0]), 2) + pow((pos[1] - vortex_p2[1]), 2);
+					wz(cell) = (real)1 / (real)0.3 * ((real)2 - pr1 / (real)0.09) * exp((real)0.5 * ((real)1 - pr1 / (real)0.09));
+					wz(cell) += (real)1 / (real)0.3 * ((real)2 - pr2 / (real)0.09) * exp((real)0.5 * ((real)1 - pr2 / (real)0.09));
 				}
 			);
 
-			//one cell
-			/*VectorDi face = MathFunc::Vi<d>(center[0]*scale, center[1]*scale);
-			face_fixed(0,face) = true;
-			face_fixed(1, face) = true;
-			initial_vel(0, face) = 0.0;
-			initial_vel(1, face) = 0.0;
-			vol(0, face) = 0;
-			vol(1, face) = 0;
-			VectorDi cell = MathFunc::Vi<d>(center[0]*scale, center[1]*scale);
-			fixed(cell) = true;*/
+			Field<real, d> sol(grid.counts, (real)0);
+			FaceField<real, d> alpha(grid.counts, (real)1);
 
+			//MaskedPoissonMapping<real, d> poisson(grid, alpha, wz, sol, fluid.psi_N); //what is bc????
+			//poisson.Build_And_Solve(); // a different method now?
+
+
+			//grid.Exec_Faces(
+			//	[&](const int axis, const VectorDi face) {
+			//		const VectorD pos = grid.Face_Center(axis, face);
+
+			//		/*if (fluid->mac_grid.Is_Axial_Boundary_Face(axis, face)) { continue; }
+			//		const VectorDi cell_left = MacGrid<d>::Face_Left_Cell(axis, face);
+			//		const VectorDi cell_right = MacGrid<d>::Face_Right_Cell(axis, face);*/
+
+
+			//		template<int d> bool MacGrid<d>::Is_Axial_Boundary_Face(const int axis, const VectorDi & face) const
+			//		{
+			//			return face[axis] == 0 || face[axis] == face_grids[axis].node_counts[axis] - 1;
+			//		}v
+
+			//			if (axis == 0) {
+			//				initial_vel(1, face) = (sol(cell_right) - sol(cell_left)) / grid.dx;
+			//			}
+			//			else if (axis == 1) {
+			//				initial_vel(0, face) = (sol(cell_left) - sol(cell_right)) / grid.dx;
+			//			}
+
+			//	};
 
 			fluid.Init(fixed, vol, face_fixed, initial_vel);
 		}
