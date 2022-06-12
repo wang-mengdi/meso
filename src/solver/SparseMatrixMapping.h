5aa6710051998fe75a3c185529cdc6ce833b7b8a
wmdcstdio <mengdi.wang.gr@dartmouth.edu>
2022-06-11 22:17:36 -0400

    add lvalue constructor for sparsematrixmapping

--- src/solver/SparseMatrixMapping.h
+++ src/solver/SparseMatrixMapping.h
@@ -55,7 +55,7 @@ namespace Meso {
 			thrust::copy(A_ptr, A_ptr + m + 1, ptr.begin());
 			thrust::copy(A_col, A_col + nnz, col.begin());
 			thrust::copy(A_val, A_val + nnz, val.begin());
-		}
+		}SparseMatrixMapping(const Eigen::SparseMatrix<T, Eigen::RowMajor, int>& A) :SparseMatrixMapping<T, side>(std::move(A)) {}
 		~SparseMatrixMapping() {
 			if (cusparseHandle) cusparseDestroy(cusparseHandle);
 		}
