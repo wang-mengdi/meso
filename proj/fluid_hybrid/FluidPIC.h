97e3472c52a49cbc5921d399326c9a5a164add26
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2022-07-19 19:59:24 +0800

    add reseeding for pic and picimpulse

--- proj/fluid_hybrid/FluidPIC.h
+++ proj/fluid_hybrid/FluidPIC.h
@@ -33,31 +33,61 @@ namespace Meso {
 		Array<VectorD> particles_pos;
 		Array<VectorD> particles_vel;
 
-		void Clamp_Particles_Pos() {
-			Grid<d> grid = velocity_host.grid;
-			VectorD pos_min = grid.Node_Min();
-			VectorD pos_max = grid.Node_Max();
+		int Delete_Particles(const Array<bool>& is_deleted){
 			const int particles_num = particles_pos.size();
+			int i = 0;
 			for (int p = 0; p < particles_num; p++)
-				for (int axis = 0; axis < d; axis++)
-					particles_pos[p][axis]= std::min(pos_max(axis), std::max(pos_min(axis), particles_pos[p][axis]));
+				if (!is_deleted[p])
+				{
+					particles_pos[i] = particles_pos[p];
+					particles_vel[i] = particles_vel[p];
+					++i;
+				}
+			particles_pos.resize(i);
+			particles_vel.resize(i);
+			return i;
 		}
 
-		void Reinit_Particles(){
-			particles_pos.clear();
-			particles_vel.clear();
+		bool Out_Of_Range(const VectorD& particle_pos, const VectorD&pos_min, const VectorD& pos_max) {
+			for (int axis = 0; axis < d; axis++)
+				if (particle_pos[axis]<pos_min[axis] || particle_pos[axis]>pos_max[axis])
+					return true;
+			return false;
+		}
+
+		void Delete_Out_Of_Range_Particles(){
+			const int particles_num = particles_pos.size();
+			Array<bool> is_deleted(particles_num, false);
+			VectorD pos_min = velocity_host.grid.Node_Min();
+			VectorD pos_max = velocity_host.grid.Node_Max();
+			for (int p = 0; p < particles_num; p++)
+				is_deleted[p] = Out_Of_Range(particles_pos[p], pos_min, pos_max);
+			Delete_Particles(is_deleted);
+		}
+
+		void Init_Particles(){
+
 			Grid<d> grid = velocity_host.grid;
+			const int particles_per_cell = (1 << d);
+			const int particles_num = grid.Counts().prod() * particles_per_cell;
+			particles_pos.resize(particles_num);
+			particles_vel.resize(particles_num);
+
+			int p = 0;
+			VectorD pos_min = grid.Node_Min();
+			VectorD pos_max = grid.Node_Max();
 			grid.Iterate_Nodes([&](const VectorDi& cell) {
 				const VectorD cell_center_pos = grid.Position(cell);
-				const int particles_per_cell = (1 << d);
 				for (int i = 0; i < particles_per_cell; i++)
 				{
-					VectorD particle_pos = cell_center_pos + VectorD::Random() * grid.dx * 0.5;
-					particles_pos.push_back(particle_pos);
-					particles_vel.push_back(VectorD::Zero());
+					VectorD delta_pos= VectorD::Random() * grid.dx * 0.5;
+					while (Out_Of_Range(cell_center_pos + delta_pos, pos_min, pos_max))
+						delta_pos = VectorD::Random() * grid.dx * 0.5;
+					particles_pos[p] = cell_center_pos + delta_pos;
+					particles_vel[p] = VectorD::Zero();
+					++p;
 				}
 				});
-			Clamp_Particles_Pos();
 		}
 
 		void Init(Field<bool, d>& fixed, FaceField<real, d>& vol, FaceField<bool, d>& face_fixed, FaceField<real, d>& initial_velocity) {
@@ -74,7 +104,7 @@ namespace Meso {
 			MGPCG.Init(&poisson, &MG_precond, false, -1, 1e-6);
 			Grid_Operation();
 			
-			Reinit_Particles();
+			Init_Particles();
 		}
 
 		virtual real CFL_Time(const real cfl) {
@@ -122,6 +152,55 @@ namespace Meso {
 			VTKFunc::Write_VTU_Particles<d>(particles_pos, particles_vel, particles_path.string());
 		}
 
+		void Reseeding() {
+			Grid<d> grid = velocity_host.grid;
+			Field<int, d> particles_num_in_cell;
+			particles_num_in_cell.Init(grid, 0);
+			int particles_num = particles_pos.size();
+			Array<VectorDi> particle_to_cell(particles_num);
+			
+			VectorD pos_min = grid.Node_Min();
+			real dx = grid.dx;
+			for (int p = 0; p < particles_num; p++)
+			{
+				VectorDi cell = ((particles_pos[p] - pos_min) / dx).template cast<int>();
+				particle_to_cell[p] = cell;
+				++particles_num_in_cell(cell);
+			}
+			
+			Array<bool> is_deleted(particles_num, false);
+			const int count_max = (1 << (d + 1));
+			for (int p = 0; p < particles_num; p++)
+				if (particles_num_in_cell(particle_to_cell[p]) > count_max){
+					is_deleted[p] = true;
+					--particles_num_in_cell(particle_to_cell[p]);
+				}
+			particles_num = Delete_Particles(is_deleted);
+
+			int add_particles_num = 0;
+			particles_num_in_cell.Iterate_Nodes([&](const VectorDi cell) {
+				if (particles_num_in_cell(cell) < 2)
+					add_particles_num += 2 - particles_num_in_cell(cell);
+				});
+			
+			particles_pos.resize(particles_num + add_particles_num);
+			particles_vel.resize(particles_num + add_particles_num);
+			VectorD pos_max = grid.Node_Max();
+			particles_num_in_cell.Iterate_Nodes([&](const VectorDi cell) {
+				while (particles_num_in_cell(cell) < 2){
+					VectorD cell_center_pos = grid.Position(cell);
+					VectorD delta_pos = VectorD::Random() * grid.dx * 0.5;
+					while (Out_Of_Range(cell_center_pos + delta_pos, pos_min, pos_max))
+						delta_pos = VectorD::Random() * grid.dx * 0.5;
+					particles_pos[particles_num] = cell_center_pos + delta_pos;
+					particles_vel[particles_num] = VectorD::Zero();
+					++particles_num;
+					++particles_num_in_cell(cell);
+				}
+				});
+
+		}
+
 		void Grid_To_Particles(){
 			const int particles_num = particles_pos.size();
 			for (int p = 0; p < particles_num; p++)
@@ -132,7 +211,7 @@ namespace Meso {
 			const int particles_num = particles_pos.size();
 			for (int p = 0; p < particles_num; p++)
 				particles_pos[p] += particles_vel[p] * dt;
-			Clamp_Particles_Pos();
+			Delete_Out_Of_Range_Particles();
 		}
 
 		void Particles_To_Grid(){
@@ -206,6 +285,7 @@ namespace Meso {
 		}
 
 		virtual void Advance(DriverMetaData& metadata) {
+			Reseeding();
 			Grid_To_Particles();
 			Particles_Operation(metadata.dt);
 			Particles_To_Grid();
--- proj/fluid_hybrid/FluidPICImpulse.h
+++ proj/fluid_hybrid/FluidPICImpulse.h
@@ -40,41 +40,72 @@ namespace Meso {
 		int reinit_cnt = 0;
 		int reinit_threshold = 1;
 
-		void Clamp_Particles_Pos() {
-			Grid<d> grid = velocity_host.grid;
-			VectorD pos_min = grid.Node_Min();
-			VectorD pos_max = grid.Node_Max();
+		int Delete_Particles(const Array<bool>& is_deleted) {
 			const int particles_num = particles_pos.size();
+			int i = 0;
 			for (int p = 0; p < particles_num; p++)
-				for (int axis = 0; axis < d; axis++)
-					particles_pos[p][axis] = std::min(pos_max(axis), std::max(pos_min(axis), particles_pos[p][axis]));
+				if (!is_deleted[p])
+				{
+					particles_pos[i] = particles_pos[p];
+					particles_vel[i] = particles_vel[p];
+					particles_init_impulse[i] = particles_init_impulse[p];
+					particles_impulse[i] = particles_impulse[p];
+					particles_inv_trans_def_grad[i] = particles_inv_trans_def_grad[p];
+					++i;
+				}
+			particles_pos.resize(i);
+			particles_vel.resize(i);
+			particles_init_impulse.resize(i);
+			particles_impulse.resize(i);
+			particles_inv_trans_def_grad.resize(i);
+			return i;
+		}
+
+		bool Out_Of_Range(const VectorD& particle_pos, const VectorD& pos_min, const VectorD& pos_max) {
+			for (int axis = 0; axis < d; axis++)
+				if (particle_pos[axis]<pos_min[axis] || particle_pos[axis]>pos_max[axis])
+					return true;
+			return false;
+		}
+
+		void Delete_Out_Of_Range_Particles() {
+			const int particles_num = particles_pos.size();
+			Array<bool> is_deleted(particles_num, false);
+			VectorD pos_min = velocity_host.grid.Node_Min();
+			VectorD pos_max = velocity_host.grid.Node_Max();
+			for (int p = 0; p < particles_num; p++)
+				is_deleted[p] = Out_Of_Range(particles_pos[p], pos_min, pos_max);
+			Delete_Particles(is_deleted);
 		}
 
 		void Reinit_Particles() {
-			particles_pos.clear();
-			particles_vel.clear();
-			particles_init_impulse.clear();
-			particles_impulse.clear();
-			particles_inv_trans_def_grad.clear();
-
 			Grid<d> grid = velocity_host.grid;
+			const int particles_per_cell = (1 << d);
+			const int particles_num = grid.Counts().prod() * particles_per_cell;
+			particles_pos.resize(particles_num);
+			particles_vel.resize(particles_num);
+			particles_init_impulse.resize(particles_num);
+			particles_impulse.resize(particles_num);
+			particles_inv_trans_def_grad.resize(particles_num);
+
+			int p = 0;
+			VectorD pos_min = grid.Node_Min();
+			VectorD pos_max = grid.Node_Max();
 			grid.Iterate_Nodes([&](const VectorDi& cell) {
 				const VectorD cell_center_pos = grid.Position(cell);
-				const int particles_per_cell = (1 << d);
 				for (int i = 0; i < particles_per_cell; i++)
 				{
-					VectorD particle_pos = cell_center_pos + VectorD::Random() * grid.dx * 0.5;
-					particles_pos.push_back(particle_pos);
-					particles_inv_trans_def_grad.push_back(MatrixD::Identity());
+					VectorD delta_pos = VectorD::Random() * grid.dx * 0.5;
+					while (Out_Of_Range(cell_center_pos + delta_pos, pos_min, pos_max))
+						delta_pos = VectorD::Random() * grid.dx * 0.5;
+					particles_pos[p] = cell_center_pos + delta_pos;
+					particles_vel[p] = IntpLinearClamp::Face_Vector(velocity_host, particles_pos[p]);
+					particles_init_impulse[p] = particles_vel[p];
+					particles_impulse[p] = particles_vel[p];
+					particles_inv_trans_def_grad[p] = MatrixD::Identity();
+					++p;
 				}
 				});
-			Clamp_Particles_Pos();
-			const int particles_num = particles_pos.size();
-			for (int p = 0; p < particles_num; p++) {
-				particles_vel.push_back(IntpLinearClamp::Face_Vector(velocity_host, particles_pos[p]));
-				particles_init_impulse.push_back(particles_vel[p]);
-				particles_impulse.push_back(particles_vel[p]);
-			}
 		}
 
 		void Init(Field<bool, d>& fixed, FaceField<real, d>& vol, FaceField<bool, d>& face_fixed, FaceField<real, d>& initial_velocity, int _reinit_threshold) {
@@ -91,8 +122,8 @@ namespace Meso {
 			MGPCG.Init(&poisson, &MG_precond, false, -1, 1e-6);
 			Grid_Operation();
 
-			Reinit_Particles();
 			reinit_threshold = _reinit_threshold;
+			Reinit_Particles();
 		}
 
 		virtual real CFL_Time(const real cfl) {
@@ -138,6 +169,61 @@ namespace Meso {
 			std::string particles_name = fmt::format("particles{:04d}.vtu", metadata.current_frame);
 			bf::path particles_path = metadata.base_path / bf::path(particles_name);
 			VTKFunc::Write_VTU_Particles<d>(particles_pos, particles_vel, particles_path.string());
+		}
+
+		void Reseeding() {
+			Grid<d> grid = velocity_host.grid;
+			Field<int, d> particles_num_in_cell;
+			particles_num_in_cell.Init(grid, 0);
+			int particles_num = particles_pos.size();
+			Array<VectorDi> particle_to_cell(particles_num);
+
+			VectorD pos_min = grid.Node_Min();
+			real dx = grid.dx;
+			for (int p = 0; p < particles_num; p++)
+			{
+				VectorDi cell = ((particles_pos[p] - pos_min) / dx).template cast<int>();
+				particle_to_cell[p] = cell;
+				++particles_num_in_cell(cell);
+			}
+
+			Array<bool> is_deleted(particles_num, false);
+			const int count_max = (1 << (d + 1));
+			for (int p = 0; p < particles_num; p++)
+				if (particles_num_in_cell(particle_to_cell[p]) > count_max) {
+					is_deleted[p] = true;
+					--particles_num_in_cell(particle_to_cell[p]);
+				}
+			particles_num = Delete_Particles(is_deleted);
+
+			int add_particles_num = 0;
+			particles_num_in_cell.Iterate_Nodes([&](const VectorDi cell) {
+				if (particles_num_in_cell(cell) < 2)
+					add_particles_num += 2 - particles_num_in_cell(cell);
+				});
+
+			particles_pos.resize(particles_num + add_particles_num);
+			particles_vel.resize(particles_num + add_particles_num);
+			particles_init_impulse.resize(particles_num + add_particles_num);
+			particles_impulse.resize(particles_num + add_particles_num);
+			particles_inv_trans_def_grad.resize(particles_num + add_particles_num);
+			VectorD pos_max = grid.Node_Max();
+			particles_num_in_cell.Iterate_Nodes([&](const VectorDi cell) {
+				while (particles_num_in_cell(cell) < 2) {
+					VectorD cell_center_pos = grid.Position(cell);
+					VectorD delta_pos = VectorD::Random() * grid.dx * 0.5;
+					while (Out_Of_Range(cell_center_pos + delta_pos, pos_min, pos_max))
+						delta_pos = VectorD::Random() * grid.dx * 0.5;
+					particles_pos[particles_num] = cell_center_pos + delta_pos;
+					particles_vel[particles_num] = IntpLinearClamp::Face_Vector(velocity_host, particles_pos[particles_num]);
+					particles_init_impulse[particles_num] = particles_vel[particles_num];
+					particles_impulse[particles_num] = particles_vel[particles_num];
+					particles_inv_trans_def_grad[particles_num] = MatrixD::Identity();
+					++particles_num;
+					++particles_num_in_cell(cell);
+				}
+				});
+
 		}
 
 		void Grid_To_Particles(const double dt) {
@@ -198,7 +284,7 @@ namespace Meso {
 			const int particles_num = particles_pos.size();
 			for (int p = 0; p < particles_num; p++)
 				particles_pos[p] += particles_vel[p] * dt;
-			Clamp_Particles_Pos();
+			Delete_Out_Of_Range_Particles();
 		}
 
 		void Particles_To_Grid() {
@@ -272,17 +358,19 @@ namespace Meso {
 		}
 
 		virtual void Advance(DriverMetaData& metadata) {
-			Grid_To_Particles(metadata.dt);
-			Particles_Operation(metadata.dt); 
-			Particles_To_Grid(); 
-			Grid_Operation(); 
-
-			reinit_cnt++;
 			if (reinit_cnt == reinit_threshold)
 			{
 				Reinit_Particles();
 				reinit_cnt = 0;
 			}
+			else
+				Reseeding();
+			reinit_cnt++;
+
+			Grid_To_Particles(metadata.dt);
+			Particles_Operation(metadata.dt); 
+			Particles_To_Grid(); 
+			Grid_Operation(); 
 		}
 	};
 }
