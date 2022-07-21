a771ddffbcdaadf08b0fa0c002869befbdf29572
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2022-07-21 10:29:21 +0800

    add taylor vortex for fluidImpulse

--- proj/fluid_euler/FluidImpulseInitializer.h
+++ proj/fluid_euler/FluidImpulseInitializer.h
@@ -23,6 +23,7 @@ namespace Meso {
 			case 0:Case_0(j, fluid); break;
 			case 1:Case_1(j, fluid); break;
 			case 2:Case_2(j, fluid); break;
+			case 3:Case_3(j, fluid); break;
 			default:Assert(false, "test {} not exist", test); break;
 			}
 		}
@@ -107,6 +108,79 @@ namespace Meso {
 						face_fixed(axis, face) = true;
 						initial_vel(axis, face) = 0.0;
 						vol(axis, face) = 0;
+					}
+				}
+			);
+
+			fluid.Init(fixed, vol, face_fixed, initial_vel);
+		}
+
+		// taylor vortex
+		void Case_3(json& j, FluidImpulse<d>& fluid) {
+			int scale = Json::Value(j, "scale", 32);
+			const real pi = 3.1415926;
+			real dx = 2.0 * pi / scale;
+			VectorDi grid_size = scale * MathFunc::Vi<d>(1, 1, 1);
+			Grid<d> grid(grid_size, dx, VectorD::Zero(), CENTER);
+			MaskedPoissonMapping<real, d> poisson;
+			VCycleMultigridIntp<real, d> MG_precond;
+			ConjugateGradient<real> MGPCG;
+
+			Eigen::Matrix<int, 3, 2> bc_width;
+			Eigen::Matrix<real, 3, 2> bc_val;
+			bc_width << -1, -1, -1, -1, -1, -1;
+			bc_val << 0, 0, 0, 0, 0, 0;
+
+			GridEulerFunc::Set_Boundary(grid, bc_width, bc_val, fixed, vol, face_fixed, initial_vel);
+
+
+			VectorD vortex_p1, vortex_p2;
+			// two vortices are 0.81 apart
+			vortex_p1[0] = grid.Center()[0] + (real)0.405;
+			vortex_p2[0] = grid.Center()[0] - (real)0.405;
+			vortex_p1[1] = grid.Center()[1];
+			vortex_p2[1] = vortex_p1[1];
+
+			Field<real, d> wz_host(grid);
+
+			grid.Exec_Nodes(
+				[&](const VectorDi cell) {
+					const VectorD pos = grid.Position(cell);
+					real pr1 = pow((pos[0] - vortex_p1[0]), 2) + pow((pos[1] - vortex_p1[1]), 2);
+					real pr2 = pow((pos[0] - vortex_p2[0]), 2) + pow((pos[1] - vortex_p2[1]), 2);
+					wz_host(cell) = (real)1 / (real)0.3 * ((real)2 - pr1 / (real)0.09) * exp((real)0.5 * ((real)1 - pr1 / (real)0.09));
+					wz_host(cell) += (real)1 / (real)0.3 * ((real)2 - pr2 / (real)0.09) * exp((real)0.5 * ((real)1 - pr2 / (real)0.09));
+					wz_host(cell) *= -grid.dx * grid.dx;
+				}
+			);
+			FieldDv<real, d> wz;
+			wz = wz_host;
+
+			FieldDv<real, d> sol(grid.Counts(), (real)0);
+			FaceField<real, d> alpha(grid.Counts(), (real)1);
+
+			poisson.Init(fixed, alpha);
+			MG_precond.Init_Poisson(poisson, 2, 2);
+			MGPCG.Init(&poisson, &MG_precond, false, -1, 1e-6);
+
+			auto [iter, error] = MGPCG.Solve(sol.Data(), wz.Data());
+			Info("iter is {}, error is {}", iter, error);
+
+			Field<real, d> sol_host = sol;
+			grid.Exec_Faces(
+				[&](const int axis, const VectorDi face) {
+					const VectorD pos = grid.Face_Center(axis, face);
+
+					if (face[axis] == 0 || face[axis] == grid.Counts()[axis]) { return; }
+
+					const VectorDi cell_left = face - VectorDi::Unit(axis);
+					const VectorDi cell_right = face;
+
+					if (axis == 0) {
+						initial_vel(1, face) = (sol_host(cell_right) - sol_host(cell_left)) / grid.dx;
+					}
+					else if (axis == 1) {
+						initial_vel(0, face) = (sol_host(cell_left) - sol_host(cell_right)) / grid.dx;
 					}
 				}
 			);
