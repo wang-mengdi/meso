6e82b6fb4c6f49fbbd805c770a2799b4a7e52bf3
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-11-01 19:17:24 -0400

    test all cases

--- proj/_tests_dec_system/main.cu
+++ proj/_tests_dec_system/main.cu
@@ -16,10 +16,6 @@
 using namespace Meso;
 
 int main(){
-    Test_MGPCG_Dirichlet_Neumann<float>(Vector3i(256, 256, 256), true);
-    return 0;
-
-
     Test_Exterior_Derivative_Cell<float, 2, ExteriorDerivativePadding0>(Vector2i(140, 530));
     Test_Exterior_Derivative_Cell<double, 2, ExteriorDerivativePadding0>(Vector2i(64, 37));
     Test_Exterior_Derivative_Cell<float, 3, ExteriorDerivativePadding0>(Vector3i(32, 47, 64));
@@ -67,18 +63,18 @@ int main(){
     Test_MGPCG<float>(Vector3i(256, 256, 256));
     Test_MGPCG<double>(Vector3i(128, 132, 128));//128,129,128 padded
 
-    Test_MGPCG_Dirichlet<double>(Vector2i(256, 256), true);
-    Test_MGPCG_Dirichlet<double>(Vector2i(512, 512), true);
+    Test_MGPCG_Dirichlet<double>(Vector2i(256, 256), false);
+    Test_MGPCG_Dirichlet<double>(Vector2i(512, 512), false);
     Test_MGPCG_Dirichlet<double>(Vector3i(128, 128, 128), false);
     Test_MGPCG_Dirichlet<double>(Vector3i(256, 256, 256), false);
 
-    Test_MGPCG_Dirichlet_Neumann<double>(Vector2i(256, 256), true);
-    Test_MGPCG_Dirichlet_Neumann<double>(Vector2i(512, 512), true);
+    Test_MGPCG_Dirichlet_Neumann<double>(Vector2i(256, 256), false);
+    Test_MGPCG_Dirichlet_Neumann<double>(Vector2i(512, 512), false);
     Test_MGPCG_Dirichlet_Neumann<double>(Vector3i(128, 128, 128), false);
     Test_MGPCG_Dirichlet_Neumann<double>(Vector3i(256, 256, 256), false);
 
-    Test_MGPCG_Neumann<double>(Vector2i(256, 256), true);
-    Test_MGPCG_Neumann<double>(Vector2i(512, 512), true);
+    Test_MGPCG_Neumann<double>(Vector2i(256, 256), false);
+    Test_MGPCG_Neumann<double>(Vector2i(512, 512), false);
     Test_MGPCG_Neumann<double>(Vector3i(256, 256, 256), false);
     return 0;
 }
