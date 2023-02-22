bc622cb461f0ee5eeca7ab120d3563fcd7e51840
Fan Feng <fan.feng.gr@dartmouth.edu>
2023-02-22 14:32:22 -0500

    Add mode of output each step for driver

--- src/common/Driver.h
+++ src/common/Driver.h
@@ -24,7 +24,7 @@ namespace Meso {
 			real frame_seconds = frame_timer.Lap_Time();
 			real completed_seconds = frame_timer.Total_Time();
 			real eta = completed_seconds * (total_frames - done_frames) / done_frames;
-			Info("Frame {} in {}-{} done in {:.3f}s, ETA {:.3f}/{:.3f}s", meta_data.current_frame, meta_data.first_frame, meta_data.last_frame, frame_seconds, eta, completed_seconds + eta);
+			Info("Frame {} in {}-{} done in {:.3f}s, total time cost {:.3f}s, ETA {:.3f}/{:.3f}s", meta_data.current_frame, meta_data.first_frame, meta_data.last_frame, frame_seconds, completed_seconds, eta, completed_seconds + eta);
 		}
 		//will change timer
 		void Print_Iteration_Info(Timer& iteration_timer, const real dt, const real current_time, const real frame_time) {
@@ -46,37 +46,55 @@ namespace Meso {
 			simulator.Output(meta_data);
 			meta_data.current_frame += 1;
 
-			//run frames
-			for (int& current_frame = meta_data.current_frame; current_frame <= meta_data.last_frame; current_frame++) {
-				Timer iter_timer;
-				int next_frame = current_frame + 1;
-				meta_data.current_time = meta_data.Time_At_Frame(current_frame);
-				real frame_start_time = meta_data.current_time;
-				real next_time = meta_data.Time_At_Frame(next_frame);
-				while (true) {
-					//can return an inf
+			if (meta_data.output_each_step) {
+				while (meta_data.current_time<meta_data.total_time) {
 					meta_data.dt = simulator.CFL_Time(meta_data.cfl);
-					meta_data.dt = MathFunc::Clamp(meta_data.dt, meta_data.min_step_frame_fraction * meta_data.time_per_frame, meta_data.time_per_frame);
-					bool last_iter = false;
-					if (meta_data.current_time + meta_data.dt >= next_time) {
-						meta_data.dt = next_time - meta_data.current_time;
-						last_iter = true;
+					if (meta_data.current_time + meta_data.dt > meta_data.total_time) { 
+						meta_data.dt = meta_data.total_time - meta_data.current_time;
+						meta_data.current_time = meta_data.total_time; 
 					}
-					else if (meta_data.current_time + 2 * meta_data.dt >= next_time) {
-						meta_data.dt = (real).5 * (next_time - meta_data.current_time);
-					}
-
-
+					else{ meta_data.current_time += meta_data.dt; }
 					simulator.Advance(meta_data);
-					meta_data.current_time += meta_data.dt;
-					Print_Iteration_Info(iter_timer, meta_data.dt, meta_data.current_time - frame_start_time, meta_data.time_per_frame);
-					if (last_iter) break;
-				}
 
-				//output current frame
-				Print_Frame_Info(frame_timer, meta_data);
-				Info("Output frame {} to {}", meta_data.current_frame, meta_data.output_base_dir);
-				simulator.Output(meta_data);
+					Print_Frame_Info(frame_timer, meta_data);
+					Info("Output time {} to {}", meta_data.current_time, meta_data.output_base_dir);
+					simulator.Output(meta_data);
+					meta_data.current_frame += 1;
+				}
+			}
+			else {
+				//run frames
+				for (int& current_frame = meta_data.current_frame; current_frame <= meta_data.last_frame; current_frame++) {
+					Timer iter_timer;
+					int next_frame = current_frame + 1;
+					meta_data.current_time = meta_data.Time_At_Frame(current_frame);
+					real frame_start_time = meta_data.current_time;
+					real next_time = meta_data.Time_At_Frame(next_frame);
+					while (true) {
+						//can return an inf
+						meta_data.dt = simulator.CFL_Time(meta_data.cfl);
+						meta_data.dt = MathFunc::Clamp(meta_data.dt, meta_data.min_step_frame_fraction * meta_data.time_per_frame, meta_data.time_per_frame);
+						bool last_iter = false;
+						if (meta_data.current_time + meta_data.dt >= next_time) {
+							meta_data.dt = next_time - meta_data.current_time;
+							last_iter = true;
+						}
+						else if (meta_data.current_time + 2 * meta_data.dt >= next_time) {
+							meta_data.dt = (real).5 * (next_time - meta_data.current_time);
+						}
+
+
+						simulator.Advance(meta_data);
+						meta_data.current_time += meta_data.dt;
+						Print_Iteration_Info(iter_timer, meta_data.dt, meta_data.current_time - frame_start_time, meta_data.time_per_frame);
+						if (last_iter) break;
+					}
+
+					//output current frame
+					Print_Frame_Info(frame_timer, meta_data);
+					Info("Output frame {} to {}", meta_data.current_frame, meta_data.output_base_dir);
+					simulator.Output(meta_data);
+				}
 			}
 		}
 
--- src/common/MetaData.h
+++ src/common/MetaData.h
@@ -22,7 +22,8 @@ namespace Meso {
 		real cfl = 1.0;
 		real time_per_frame = 0.04;
 		real min_step_frame_fraction = 0;	//if set to 0.1, it means the minimal iteration time is 0.1*time_per_frame
-
+		bool output_each_step;				//whether or not output each step, ignoring the frame rate, each frame has duration cfl_time
+		real total_time;					//total time duration, only used when output_each_step
 		int first_frame;
 		int last_frame;
 		
@@ -52,19 +53,25 @@ namespace Meso {
 			output_base_dir = Json::Value(j, "output_base_dir", std::string("output"));
 			base_path = bf::path(output_base_dir);
 
-			fps = Json::Value(j, "fps", 25);
-			cfl = Json::Value(j, "cfl", (real)1.0);
-			time_per_frame = 1.0 / fps;
-			min_step_frame_fraction = Json::Value(j, "min_step_frame_fraction", (real)0);
+			output_each_step = Json::Value(j, "output_each_step", false);
+			if (output_each_step) {
+				total_time = Json::Value(j, "total_time", (real)1);
+			}
+			else {
+				fps = Json::Value(j, "fps", 25);
+				cfl = Json::Value(j, "cfl", (real)1.0);
+				time_per_frame = 1.0 / fps;
+				min_step_frame_fraction = Json::Value(j, "min_step_frame_fraction", (real)0);
+				last_frame = Json::Value(j, "last_frame", fps * 10);
+				dt = 1.0 / fps;
+			}
 
 			first_frame = Json::Value(j, "first_frame", 0);
-			last_frame = Json::Value(j, "last_frame", fps * 10);
 			
 			output_queue_size = Json::Value(j, "queue_size", 10);
 
 			current_frame = first_frame;
 			current_time = Time_At_Frame(current_frame);
-			dt = 1.0 / fps;
 		}
 
 		void Append_Output_Thread(std::shared_ptr<std::thread> thread_ptr) {
